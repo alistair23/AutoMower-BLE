11f91d819f5e3d5cf903748ecfe3dd071c8c628b
erikmol <erikmoller91@gmail.com>
2024-04-29 12:47:06 +0200

    Major rewrite to simplify new features (#15)
    I've moved away from the request.py/response.py to something called protocol.py and protocol.json to simplify addition of new features. Also moved test cases to a separate folder.
    
    There are still some things to fix before this PR is complete, but marking it as draft here now for any feedback.

---
+++ automower_ble/protocol.py
@@ -0,0 +1,523 @@
+import binascii
+from .helpers import crc
+from enum import Enum
+import asyncio
+import logging
+import json
+from importlib.resources import files
+from bleak import BleakClient
+from bleak.backends.characteristic import BleakGATTCharacteristic
+
+logger = logging.getLogger(__name__)
+
+
+class ModeOfOperation(Enum):
+    # ProtocolTypes$IMowerAppMowerMode, used in modeOfOperation: 4586, 1
+    # Comments from: https://developer.husqvarnagroup.cloud/apis/Automower+Connect+API?tab=status%20description%20and%20error%20codes#user-content-mode
+    AUTO = 0
+    MANUAL = 1
+    HOME = 2  # Mower goes home and parks forever. Week schedule is not used. Cannot be overridden with forced mowing.
+    DEMO = 3  # Same as main area, but shorter times. No blade operation
+    POI = 4
+
+
+class MowerState(Enum):
+    # ProtocolTypes$IMowerAppState, used in mowerState: 4586, 2
+    # Comments from: https://developer.husqvarnagroup.cloud/apis/Automower+Connect+API?tab=status%20description%20and%20error%20codes#user-content-state
+    OFF = 0  # Mower is turned off.
+    WAIT_FOR_SAFETYPIN = 1
+    STOPPED = 2  # Mower is stopped requires manual action.
+    FATAL_ERROR = 3
+    PENDING_START = 4
+    PAUSED = 5  # Mower has been paused by user.
+    IN_OPERATION = 6  # See value in activity for status.
+    RESTRICTED = (
+        7  # Mower can currently not mow due to week calender, or override park.
+    )
+    ERROR = 8  # An error has occurred. Check errorCode. Mower requires manual action.
+
+
+class MowerActivity(Enum):
+    # ProtocolTypes$IMowerAppActivity, used in mowerActivity: 4586, 3
+    # Comments from: https://developer.husqvarnagroup.cloud/apis/Automower+Connect+API?tab=status%20description%20and%20error%20codes#user-content-activity
+    NONE = 0
+    CHARGING = 1  # Mower is charging in station due to low battery.
+    GOING_OUT = 2
+    MOWING = 3  # Mower is mowing lawn. If in demo mode the blades are not in operation.
+    GOING_HOME = 4  # Mower is going home to the charging station.
+    PARKED = 5
+    STOPPED_IN_GARDEN = 6  # Mower has stopped. Needs manual action to resume
+
+
+class Command:
+    def __init__(self, channel_id: int, parameter: dict):
+        self.channel_id = channel_id
+
+        self.major = parameter["major"]
+        self.minor = parameter["minor"]
+
+        if "requestType" in parameter:
+            self.request_data_type = parameter["requestType"]
+        else:
+            self.request_data_type = None
+
+        if not isinstance(parameter["responseType"], dict):  # Always wrap in list
+            self.response_data_type = {"response": parameter["responseType"]}
+        else:
+            self.response_data_type = parameter["responseType"]
+        self.request_data = bytearray()
+
+    def generate_request(self, **kwargs) -> bytearray:
+        self.request_data = bytearray(18)
+        self.request_data[0] = 0x02  # Hard coded value
+        self.request_data[1] = 0xFD  # Hard coded value
+        self.request_data[2] = 0x00  # Length, Updated later
+        self.request_data[3] = 0x00  # Hard coded value
+
+        # ChannelID
+        id = self.channel_id.to_bytes(4, byteorder="little")
+        self.request_data[4] = id[0]
+        self.request_data[5] = id[1]
+        self.request_data[6] = id[2]
+        self.request_data[7] = id[3]
+
+        self.request_data[8] = (
+            0x01  # The third argument to e(int i10, int i11, boolean z10, T t10). Almost always 1
+        )
+
+        self.request_data[9] = 0x00  # CRC, Updated later
+        self.request_data[10] = 0x00  # Hard coded value
+        self.request_data[11] = 0xAF  # Hard coded value
+
+        major_bytes = self.major.to_bytes(2, byteorder="little")
+
+        self.request_data[12] = major_bytes[0]
+        self.request_data[13] = major_bytes[1]
+        self.request_data[14] = self.minor
+
+        self.request_data[15] = 0x00  # Hard coded value
+
+        # Byte 16 represents length of request data type
+        request_length = 0
+        request_data = bytearray()
+        if self.request_data_type is not None:
+            for request_name, request_type in self.request_data_type.items():
+                if request_name not in kwargs:
+                    raise ValueError(
+                        "Missing request parameter: "
+                        + request_name
+                        + " for command ("
+                        + str(self.major)
+                        + ", "
+                        + str(self.minor)
+                        + ")"
+                    )
+
+                if request_type == "uint32":
+                    request_length += 4
+                    request_data += kwargs[request_name].to_bytes(4, byteorder="little")
+                elif request_type == "uint16":
+                    request_length += 2
+                    request_data += kwargs[request_name].to_bytes(2, byteorder="little")
+                elif request_type == "uint8":
+                    request_length += 1
+                    request_data += kwargs[request_name].to_bytes(1, byteorder="little")
+                else:
+                    raise ValueError("Unknown request type: " + self.request_type)
+        self.request_data[16] = request_length
+
+        self.request_data[17] = 0x00  # Hard coded value
+
+        if request_length > 0:
+            self.request_data += request_data
+
+        self.request_data[2] = len(self.request_data) - 2  # Length
+
+        self.request_data[9] = crc(self.request_data, 1, 8)  # CRC
+
+        # Two last bytes are crc and 0x03
+        self.request_data.append(crc(self.request_data, 1, len(self.request_data) - 1))
+        self.request_data.append(0x03)  # Hard coded value
+
+        return self.request_data
+
+    def parse_response(self, response_data: bytearray) -> int | None:
+        response_length = response_data[17]
+        data = response_data[19 : 19 + response_length]
+        response = dict()
+        dpos = 0  # data position
+        for name, dtype in self.response_data_type.items():
+            if dtype == "no_response":
+                return None
+            elif (dtype == "tUnixTime") or (dtype == "uint32"):
+                response[name] = int.from_bytes(
+                    data[dpos : dpos + 4], byteorder="little"
+                )
+                dpos += 4
+            elif dtype == "uint16":
+                response[name] = int.from_bytes(
+                    data[dpos : dpos + 2], byteorder="little"
+                )
+                dpos += 2
+            elif (dtype == "uint8") or (dtype == "bool"):
+                response[name] = data[dpos]
+                dpos += 1
+            else:
+                raise ValueError("Unknown data type: " + dtype)
+        if dpos != len(data):
+            raise ValueError(
+                "Data length mismatch. Read %d bytes of %d" % (dpos, len(data))
+            )
+
+        return response
+
+    def validate_response(self, response_data: bytearray) -> bool:
+        if response_data[0] != 0x02:
+            return False
+
+        if response_data[1] != 0xFD:
+            return False
+
+        if response_data[3] != 0x00:
+            return False
+
+        id = self.channel_id.to_bytes(4, byteorder="little")
+        if response_data[4] != id[0]:
+            return False
+        if response_data[5] != id[1]:
+            return False
+        if response_data[6] != id[2]:
+            return False
+        if response_data[7] != id[3]:
+            return False
+
+        if response_data[8] != 0x01:
+            # This is a valid config, but we don't support it
+            # return m1656b(decodeState, c10786f);
+            return False
+
+        if response_data[9] != crc(response_data, 1, 8):
+            return False
+
+        if response_data[10] != 0x01:
+            return False
+
+        if response_data[11] != 0xAF:
+            return False
+
+        major_bytes = self.major.to_bytes(4, byteorder="little")
+        if response_data[12] != major_bytes[0]:
+            return False
+        if response_data[13] != major_bytes[1]:
+            return False
+        if response_data[14] != self.minor:
+            return False
+
+        if response_data[15] != 0x00:
+            return False
+
+        if response_data[16] != 0x00:
+            return False
+
+        return True
+
+
+class BLEClient:
+    def __init__(self, channel_id: int, address, pin=None):
+        self.channel_id = channel_id
+        self.address = address
+        self.pin = pin
+        self.MTU_SIZE = 20
+
+        self.queue = asyncio.Queue()
+
+        with files("automower_ble").joinpath("protocol.json").open("r") as f:
+            self.protocol = json.load(f)  # Load the JSON file
+
+    async def _get_response(self):
+        try:
+            data = await asyncio.wait_for(self.queue.get(), timeout=10)
+
+        except TimeoutError:
+            logger.error("Unable to get response from device: '%s'", self.address)
+            if self.is_connected():
+                await self.disconnect()
+            return None
+
+        return data
+
+    async def _write_data(self, data):
+        logger.info("Writing: " + str(binascii.hexlify(data)))
+
+        chunk_size = self.MTU_SIZE - 3
+        for chunk in (
+            data[i : i + chunk_size] for i in range(0, len(data), chunk_size)
+        ):
+            await self.client.write_gatt_char(self.write_char, chunk, response=False)
+
+        logger.debug("Finished writing")
+
+    async def _read_data(self):
+        data = await self._get_response()
+
+        if data is None:
+            return None
+
+        if len(data) < 3:
+            # We got such a small amount of data, let's try again
+            data = data + await self._get_response()
+
+            if len(data) < 3:
+                # Something is wrong
+                return None
+
+        length = data[2] + 4
+
+        logger.debug("Waiting for %d bytes", length)
+
+        while len(data) != length:
+            try:
+                data = data + await asyncio.wait_for(self.queue.get(), timeout=5)
+            except TimeoutError:
+                logger.error(
+                    "Unable to get full response from device: '%s', currently have"
+                    + str(binascii.hexlify(data)),
+                    self.address,
+                )
+                return None
+
+        logger.info("Final response: " + str(binascii.hexlify(data)))
+
+        return data
+
+    async def _request_response(self, request_data):
+        i = 5
+        while i > 0:
+            try:
+                # If there are previous responses, flush them out
+                while not self.queue.empty():
+                    await self.queue.get()
+
+                await self._write_data(request_data)
+
+                response_data = await self._read_data()
+                if response_data is None:
+                    i = i - 1
+                    continue
+
+            except asyncio.exceptions.CancelledError:
+                logger.debug("Received CancelledError")
+                i = i - 1
+                continue
+
+            break
+
+        if i == 0:
+            logger.error("Unable to communicate with device: '%s'", self.address)
+            if self.is_connected():
+                await self.disconnect()
+            return None
+
+        return response_data
+
+    async def connect(self, device) -> bool:
+        """
+        Connect to a device and setup the channel
+
+        Returns True on success
+        """
+        logger.info("starting scan...")
+
+        if device is None:
+            logger.error("could not find device with address '%s'", self.address)
+            return False
+
+        logger.info("connecting to device...")
+        self.client = BleakClient(
+            device, services=["98bd0001-0b0e-421a-84e5-ddbf75dc6de4"], use_cached=True
+        )
+        await self.client.connect()
+        logger.info("connected")
+
+        logger.info("pairing device...")
+        await self.client.pair()
+        logger.info("paired")
+
+        self.client._backend._mtu_size = self.MTU_SIZE
+
+        for service in self.client.services:
+            logger.info("[Service] %s", service)
+
+            for char in service.characteristics:
+                if "read" in char.properties:
+                    try:
+                        value = await self.client.read_gatt_char(char.uuid)
+                        logger.debug(
+                            "  [Characteristic] %s (%s), Value: %r",
+                            char,
+                            ",".join(char.properties),
+                            value,
+                        )
+                    except Exception as e:
+                        logger.error(
+                            "  [Characteristic] %s (%s), Error: %s",
+                            char,
+                            ",".join(char.properties),
+                            e,
+                        )
+
+                else:
+                    logger.debug(
+                        "  [Characteristic] %s (%s)", char, ",".join(char.properties)
+                    )
+                if char.uuid == "98bd0002-0b0e-421a-84e5-ddbf75dc6de4":
+                    self.write_char = char
+
+                if char.uuid == "98bd0003-0b0e-421a-84e5-ddbf75dc6de4":
+                    self.read_char = char
+
+        async def notification_handler(
+            characteristic: BleakGATTCharacteristic, data: bytearray
+        ):
+            logger.info("Received: " + str(binascii.hexlify(data)))
+            await self.queue.put(data)
+
+        await self.client.start_notify(self.read_char, notification_handler)
+
+        await asyncio.sleep(5.0)
+
+        request = self.generate_request_setup_channel_id()
+        response = await self._request_response(request)
+        if response is None:
+            return False
+
+        ### TODO: Check response
+
+        request = self.generate_request_handshake()
+        response = await self._request_response(request)
+        if response is None:
+            return False
+
+        ### TODO: Check response
+
+        if self.pin is not None:
+            command = Command(self.channel_id, self.protocol["pin"])
+            request = command.generate_request(code=self.pin)
+            response = await self._request_response(request)
+            if response is None:
+                return False
+
+        return True
+
+    def is_connected(self) -> bool:
+        return self.client.is_connected
+
+    async def probe_gatts(self, device):
+        if device is None:
+            logger.error("could not find device with address '%s'", self.address)
+            return False
+
+        logger.info("connecting to device...")
+        client = BleakClient(
+            device, services=["98bd0001-0b0e-421a-84e5-ddbf75dc6de4"], use_cached=True
+        )
+
+        await client.connect()
+        logger.info("connected")
+
+        manufacture = None
+        model = None
+        device_type = None
+
+        for service in client.services:
+            logger.debug("[Service] %s", service)
+
+            if service.uuid == "98bd0001-0b0e-421a-84e5-ddbf75dc6de4":
+                manufacture = service.description
+
+            for char in service.characteristics:
+                if "read" in char.properties:
+                    try:
+                        value = await client.read_gatt_char(char.uuid)
+                        logger.debug(
+                            "  [Characteristic] %s (%s), Value: %r",
+                            char,
+                            ",".join(char.properties),
+                            value,
+                        )
+                    except Exception as e:
+                        logger.error(
+                            "  [Characteristic] %s (%s), Error: %s",
+                            char,
+                            ",".join(char.properties),
+                            e,
+                        )
+
+                else:
+                    logger.debug(
+                        "  [Characteristic] %s (%s)", char, ",".join(char.properties)
+                    )
+
+                if char.uuid == "00002a00-0000-1000-8000-00805f9b34fb":
+                    model = await client.read_gatt_char(char)
+
+                if char.uuid == "98bd0004-0b0e-421a-84e5-ddbf75dc6de4":
+                    device_type = await client.read_gatt_char(char)
+
+        await client.disconnect()
+
+        return (manufacture, device_type.decode(), model.decode())
+
+    async def disconnect(self):
+        """
+        Disconnect from the mower, this should be called after every
+        `connect()` before the Python script exits
+        """
+
+        await self.client.stop_notify(self.read_char)
+        await self.queue.put(None)
+
+        logger.info("disconnecting...")
+        await self.client.disconnect()
+        logger.info("disconnected")
+
+    def generate_request_setup_channel_id(self) -> bytearray:
+        """
+        Setup the channelID with an Automower, this is the first
+        command that should be sent
+        """
+        data = bytearray.fromhex("02fd160000000000002e1400000000000000004d61696e00")
+
+        # New ChannelID
+        id = self.channel_id.to_bytes(4, byteorder="little")
+        data[11] = id[0]
+        data[12] = id[1]
+        data[13] = id[2]
+        data[14] = id[3]
+
+        # CRC and end byte
+        data[9] = crc(data, 1, 8)
+        data.append(crc(data, 1, len(data) - 1))
+        data.append(0x03)
+
+        return data
+
+    def generate_request_handshake(self) -> bytearray:
+        """
+        Generate a request handshake. This should be called after
+        the channel id is set up but before other commands
+        """
+        data = bytearray.fromhex("02fd0a000000000000d00801")
+
+        id = self.channel_id.to_bytes(4, byteorder="little")
+        data[4] = id[0]
+        data[5] = id[1]
+        data[6] = id[2]
+        data[7] = id[3]
+
+        # CRCs and end byte
+        data[9] = crc(data, 1, 8)
+        data.append(crc(data, 1, len(data) - 1))
+        data.append(0x03)
+
+        return data
---
+++ tests/test_response.py
@@ -0,0 +1,78 @@
+import unittest
+import json
+from importlib.resources import files
+from automower_ble.protocol import Command, MowerState, MowerActivity
+from automower_ble.models import MowerModels
+
+
+class TestRequestMethods(unittest.TestCase):
+    def setUp(self):
+        with files("automower_ble").joinpath("protocol.json").open("r") as f:
+            self.protocol = json.load(f)  # Load the parameters to have them available
+
+    def test_decode_response_device_type(self):
+        command = Command(1197489078, parameter=self.protocol["deviceType"])
+
+        response = command.parse_response(
+            bytearray.fromhex("02fd1300b63b604701e601af5a1209000002001701c803")
+        )
+        self.assertEqual(
+            MowerModels[(response["deviceType"], response["deviceSubType"])],
+            "305",
+        )
+
+        response = command.parse_response(
+            bytearray.fromhex("02fd130038e38f0b01dc01af5a1209000002000c005903")
+        )
+        self.assertEqual(
+            MowerModels[(response["deviceType"], response["deviceSubType"])],
+            "315",
+        )
+
+    def test_decode_response_is_charging(self):
+        command = Command(1197489078, self.protocol["isCharging"])
+
+        self.assertEqual(
+            command.parse_response(
+                bytearray.fromhex("02fd1200b63b604701db01af0a101500000100011603")
+            )["response"],
+            True,
+        )
+        self.assertEqual(
+            command.parse_response(
+                bytearray.fromhex("02fd1200b63b604701db01af0a101500000100004803")
+            )["response"],
+            False,
+        )
+
+    def test_decode_response_mower_state(self):
+        command = Command(1197489078, self.protocol["mowerState"])
+
+        self.assertNotIn(
+            command.parse_response(
+                bytearray.fromhex("02fd1200b33b6047010901afea110100000100008103")
+            )["response"],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14],  # 11=unknown
+        )
+
+        command = Command(876143061, self.protocol["mowerState"])
+        self.assertEqual(
+            command.parse_response(
+                bytearray.fromhex("02fd1200d5e13834012301afea110200000100033a03")
+            )["response"],
+            MowerState.FATAL_ERROR.value,
+        )
+
+    def test_decode_response_mower_activity(self):
+        response = Command(1197489078, self.protocol["mowerActivity"])
+
+        self.assertEqual(
+            response.parse_response(
+                bytearray.fromhex("02fd1200b33b6047010901afea110200000100026403")
+            )["response"],
+            MowerActivity.GOING_OUT.value,  # 2 = goingOut
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
