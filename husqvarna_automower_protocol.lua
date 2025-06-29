bfeb2c11be0ab5c0ec59b447833206c008b48396
Kevin Salmon <11260747+KevinSalmon@users.noreply.github.com>
2025-06-29 18:57:34 +0200

    Add additional commands in lua script
    Commands extracted from the official APK and/or sniffed from a Husqvarna Automower 420

--- husqvarna_automower_protocol.lua
+++ husqvarna_automower_protocol.lua
@@ -98,6 +98,38 @@ do
 			elseif min == 0x0A then
 				minorText = "getBatteryNbrChargingCyclesRequest"
 			end
+		elseif maj == 0xB010 then -- TemperatureCommands 4272
+			subtree:add(automower_request_major_text, "TemperatureCommands")
+			if min == 0x00 then
+				minorText = "getTemperatureRequest"
+			end
+		elseif maj == 0x4611 then -- CuttingHeightCommands 4422
+			subtree:add(automower_request_major_text, "CuttingHeightCommands")
+			if min == 0x00 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x01 then
+				minorText = "subscribeEventChannelRequest"
+			elseif min == 0x02 then
+				minorText = "getCuttingHeightRequest"
+			elseif min == 0x03 then
+				minorText = "setCuttingHeightRequest"
+			elseif min == 0x04 then
+				minorText = "getAllSettingsRequest"
+			elseif min == 0x05 then
+				minorText = "getAvailableRequest"
+			elseif min == 0x06 then
+				minorText = "getHeightPercentageRequest"
+			elseif min == 0x07 then
+				minorText = "setHeightPercentageRequest"
+			elseif min == 0x08 then
+				minorText = "getDownCuttingEnabledRequest"
+			elseif min == 0x09 then
+				minorText = "setDownCuttingEnabledRequest"
+			elseif min == 0x0A then
+				minorText = "getDownCuttingAvailableRequest"
+			elseif min == 0x0C then
+				minorText = "getDownCuttingStatusRequest"
+			end
 		elseif maj == 0xEA11 then -- MowerAppCommands 4586
 			subtree:add(automower_request_major_text, "MowerAppCommands")
 			if  min == 0x0 then
@@ -114,6 +146,18 @@ do
 				minorText = "pauseRequest"
 			elseif min == 0x06 then
 				minorText = "getErrorRequest"
+			elseif min == 0x07 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x08 then
+				minorText = "isErrorConfirmableRequest"
+			elseif min == 0x09 then
+				minorText = "confirmErrorRequest"
+			elseif min == 0x0D then
+				minorText = "getMissionRequest"
+			elseif min == 0x0F then
+				minorText = "abortStartTriggerRequest"
+			elseif min == 0x13 then
+				minorText = "getInactiveReasonRequest"
 			end
 		elseif maj == 0x3212 then -- PlannerCommands 4658
 			subtree:add(automower_request_major_text, "PlannerCommands")
@@ -195,6 +239,25 @@ do
 			elseif min == 0x0B then
 				minorText = "commitTaskTransactionRequest"
 			end
+		elseif maj == 0x5412 then -- ChargingStationCommands 4692
+			subtree:add(automower_request_major_text, "ChargingStationCommands")
+			if min == 0x00 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x01 then
+				minorText = "subscribeEventChannelRequest"
+			elseif min == 0x02 then
+				minorText = "initiateNewPairingRequest"
+			elseif min == 0x03 then
+				minorText = "setMowerHouseInstalledRequest"
+			elseif min == 0x04 then
+				minorText = "getMowerHouseInstalledRequest"
+			elseif min == 0x05 then
+				minorText = "setEcoModeEnabledRequest"
+			elseif min == 0x06 then
+				minorText = "getEcoModeEnabledRequest"
+			elseif min == 0x07 then
+				minorText = "getAllSettingsRequest"
+			end
 		elseif maj == 0x5A12 then -- SystemCommands 4698
 			subtree:add(automower_request_major_text, "SystemCommands")
 			if min == 0x0 then
@@ -223,8 +286,94 @@ do
 				minorText = "getConfigVersionStringRequest"
 			elseif min == 0x0E then
 				minorText = "getProductionTimeRequest"
+			elseif min == 0x14 then
+				minorText = "getSwUpdateRequiredRequest"
 			elseif min == 0x16 then
 				minorText = "getSwPackageVersionStringRequest"
+			elseif min == 0x27 then
+				minorText = "getForcedSwUpdateRequiredRequest"
+			end
+		elseif maj == 0x6212 then -- FollowWireCommands 4706
+			subtree:add(automower_request_major_text, "FollowWireCommands")
+			if min == 0x00 then
+				minorText = "getBoundaryCorridorRequest"
+			elseif min == 0x01 then
+				minorText = "setBoundaryCorridorRequest"
+			elseif min == 0x02 then
+				minorText = "getGuideCorridorRequest"
+			elseif min == 0x03 then
+				minorText = "setGuideCorridorRequest"
+			elseif min == 0x06 then
+				minorText = "getStartingPointEnabledRequest"
+			elseif min == 0x07 then
+				minorText = "setStartingPointEnabledRequest"
+			elseif min == 0x08 then
+				minorText = "getStartingPointWireRequest"
+			elseif min == 0x09 then
+				minorText = "setStartingPointWireRequest"
+			elseif min == 0x0A then
+				minorText = "getStartingPointDistanceRequest"
+			elseif min == 0x0B then
+				minorText = "setStartingPointDistanceRequest"
+			elseif min == 0x0C then
+				minorText = "getStartingPointProportionRequest"
+			elseif min == 0x0D then
+				minorText = "setStartingPointProportionRequest"
+			elseif min == 0x0E then
+				minorText = "getNumberOfGuidesRequest"
+			elseif min == 0x0F then
+				minorText = "testStartingPointRequest"
+			elseif min == 0x10 then
+				minorText = "getCurrentDistanceRequest"
+			elseif min == 0x11 then
+				minorText = "testFollowInRequest"
+			elseif min == 0x12 then
+				minorText = "testAbortRequest"
+			elseif min == 0x13 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x14 then
+				minorText = "subscribeEventChannelRequest"
+			elseif min == 0x15 then
+				minorText = "getStartingPointSettingsRequest"
+			elseif min == 0x16 then
+				minorText = "getTestModeRequest"
+			elseif min == 0x17 then
+				minorText = "getTestStateRequest"
+            elseif min == 0x18 then
+				minorText = "getBoundaryCorridorRequestG4"
+			elseif min == 0x19 then
+				minorText = "setBoundaryCorridorRequestG4"
+			elseif min == 0x1A then
+				minorText = "getPassageCuttingEnabledRequestG4"
+			elseif min == 0x1B then
+				minorText = "setPassageCuttingEnabledRequestG4"
+			elseif min == 0x1C then
+				minorText = "getStartingPointMaxRequest"
+			elseif min == 0x1D then
+				minorText = "getTestErrorRequest"
+			end
+		elseif maj == 0x6412 then -- SearchChargingStationCommands 4708
+			subtree:add(automower_request_major_text, "SearchChargingStationCommands")
+			if min == 0x00 then
+				minorText = "getSearchTypeAvailableRequest"
+			elseif min == 0x01 then
+				minorText = "getEnabledRequest"
+			elseif min == 0x02 then
+				minorText = "setEnabledRequest"
+			elseif min == 0x03 then
+				minorText = "getDelayTimeRequest"
+			elseif min == 0x04 then
+				minorText = "setDelayTimeRequest"
+			elseif min == 0x05 then
+				minorText = "getDirectSearchChargingStationRangeRequest"
+			elseif min == 0x06 then
+				minorText = "setDirectSearchChargingStationRangeRequest"
+			elseif min == 0x07 then
+				minorText = "getAllSettingsRequest"
+			elseif min == 0x08 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x09 then
+				minorText = "subscribeEventChannelRequest"
 			end
 		elseif maj == 0x6612 then -- SpotCuttingCommands 4710
 			subtree:add(automower_request_major_text, "SpotCuttingCommands")
@@ -251,10 +400,116 @@ do
 			elseif min == 0x0A then
 				minorText = "subscribeAllEventsRequest"
 			end
+		elseif maj == 0x6812 then -- DrivingSettingsCommands 4712
+			subtree:add(automower_request_major_text, "DrivingSettingsCommands")
+			if min == 0x00 then
+				minorText = "getDrivePastWireRequest"
+			elseif min == 0x01 then
+				minorText = "setDrivePastWireRequest"
+			elseif min == 0x02 then
+				minorText = "getAllSettingsRequest"
+			elseif min == 0x09 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x0A then
+				minorText = "subscribeEventChannelRequest"
+			elseif min == 0x0B then
+				minorText = "getBounceEnabledRequest"
+			elseif min == 0x0C then
+				minorText = "setBounceEnabledRequest"
+			elseif min == 0x0D then
+				minorText = "getBounceSlopeOnlyRequest"
+			elseif min == 0x0E then
+				minorText = "setBounceSlopeOnlyRequest"
+			end
+		elseif maj == 0x6C12 then -- LeaveChargingStationCommands 4716
+			subtree:add(automower_request_major_text, "LeaveChargingStationCommands")
+			if min == 0x00 then
+				minorText = "getReversingDistanceRequest"
+			elseif min == 0x01 then
+				minorText = "setReversingDistanceRequest"
+			elseif min == 0x03 then
+				minorText = "setMinAngleSector1Request"
+			elseif min == 0x05 then
+				minorText = "setMaxAngleSector1Request"
+			elseif min == 0x07 then
+				minorText = "setMinAngleSector2Request"
+			elseif min == 0x09 then
+				minorText = "setMaxAngleSector2Request"
+			elseif min == 0x0B then
+				minorText = "setSector1ProportionRequest"
+			elseif min == 0x0C then
+				minorText = "getAllSettingsRequest"
+			elseif min == 0x0F then
+				minorText = "getAllSettingsRequestG4"
+			elseif min == 0x10 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x11 then
+				minorText = "subscribeEventChannelRequest"
+			end
+		elseif maj == 0x7612 then -- StatisticsCommands 4726
+			subtree:add(automower_request_major_text, "StatisticsCommands")
+			if min == 0x00 then
+				minorText = "getAllStatisticsRequest"
+			elseif min == 0x01 then
+				minorText = "getTotalRunningTimeRequest"
+			elseif min == 0x02 then
+				minorText = "getTotalCuttingTimeRequest"
+			elseif min == 0x03 then
+				minorText = "getTotalChargingTimeRequest"
+			elseif min == 0x04 then
+				minorText = "getTotalSearchingTimeRequest"
+			elseif min == 0x05 then
+				minorText = "getNumberOfCollisionsRequest"
+			elseif min == 0x06 then
+				minorText = "getNumberOfChargingCyclesRequest"
+			elseif min == 0x07 then
+				minorText = "getCuttingBladeUsageTimeRequest"
+			elseif min == 0x08 then
+				minorText = "resetCuttingBladeUsageTimeRequest"
+			elseif min == 0x0A then
+				minorText = "subscribeAllStatisticsRequest"
+			end
+		elseif maj == 0x7812 then -- UltrasonicCommands 4728
+			subtree:add(automower_request_major_text, "UltrasonicCommands")
+			if min == 0x00 then
+				minorText = "getAvailableRequest"
+			elseif min == 0x01 then
+				minorText = "getEnabledRequest"
+			elseif min == 0x02 then
+				minorText = "setEnabledRequest"
+			elseif min == 0x03 then
+				minorText = "subscribeAllEventsRequest"
+			elseif min == 0x04 then
+				minorText = "getAllSettingsRequest"
+			end
+		elseif maj == 0x7A12 then -- MessagesCommands 4730
+			subtree:add(automower_request_major_text, "MessagesCommands")
+			if min == 0x00 then
+				minorText = "getNumberOfMessagesRequest"
+			elseif min == 0x01 then
+				minorText = "getMessageRequest"
+			end
 		elseif maj == 0x0414 then -- AuthenticationCommands 5124
 			subtree:add(automower_request_major_text, "AuthenticationCommands")
 			if min == 0x0A then
 				minorText = "getRemainingLoginAttemptsRequest"
+			end
+		elseif maj == 0xEC14 then -- ObstacleAvoidance 5356
+			subtree:add(automower_request_major_text, "ObstacleAvoidance")
+			if min == 0x00 then
+				minorText = "subscribeSettingsEventsRequest"
+			elseif min == 0x02 then
+				minorText = "getAvailableRequest"
+			elseif min == 0x03 then
+				minorText = "setEnabledRequest"
+			elseif min == 0x04 then
+				minorText = "getEnabledRequest"
+			elseif min == 0x05 then
+				minorText = "setUseAtBoundaryRequest"
+			elseif min == 0x06 then
+				minorText = "getUseAtBoundaryRequest"
+			elseif min == 0x07 then
+				minorText = "getAllSettingsRequest"
 			end
 		elseif maj == 0x8C16 then -- SpotcuttingCommands 5772
 			subtree:add(automower_request_major_text, "SpotCuttingCommands")
@@ -280,6 +535,21 @@ do
 				minorText = "getAutoTrigCurrentRequest"
 			elseif min == 0x0A then
 				minorText = "getAutoTrigIntensityRequest"
+			end
+		elseif maj == 0x2617 then -- MobileLoopCommands 5926
+			subtree:add(automower_request_major_text, "MobileLoopCommands")
+			if min == 0x00 then
+				minorText = "getAvailableRequest"
+			elseif min == 0x01 then
+				minorText = "setAvailableRequest"
+			elseif min == 0x02 then
+				minorText = "getEnabledRequest"
+			elseif min == 0x03 then
+				minorText = "setEnabledRequest"
+			elseif min == 0x04 then
+				minorText = "getAllSettingsRequest"
+			elseif min == 0x05 then
+				minorText = "subscribeSettingsEventsRequest"
 			end
 		else
 			subtree:add(automower_request_major_text,  "NotImplementedYet")
